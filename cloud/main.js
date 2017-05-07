fc096a3f13560c7a1a8679b9f912eab1ae9e545b
NAVNEETSINGH1 <navneet.singh@careerpower.in>
2017-05-07 18:43:13 +0530

    changes for push

--- cloud/main.js
+++ cloud/main.js
@@ -4,25 +4,4 @@ Parse.Cloud.define('hello', function(req, res) {
 });
 
 
-var query = new Parse.Query(Parse.Installation);
-query.equalTo('channels', 'test-channel');
 
-Parse.Push.send(
-  {
-    where: query,
-    data: 
-    {
-      alert: 'Test',
-      badge: 1,
-      sound: 'default'
-    }
-  }, 
-  {
-    success: function() {
-      console.log('##### PUSH OK');
-    },
-    error: function(error) {
-      console.log('##### PUSH ERROR');
-    }
-  },
-  { useMasterKey: true });
