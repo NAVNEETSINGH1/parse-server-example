a0dab36e89c732357b1758cb0540010dd5594128
NAVNEET SINGH <navneet.singh@careerpower.in>
2017-05-24 23:58:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ var api = new ParseServer({
 		}
 	},
   masterKey: process.env.MASTER_KEY || 'myMasterKey', //Add your master key here. Keep it secret!
-  serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
+  serverURL: process.env.SERVER_URL || 'https://parseexampleapp.herokuapp.com/parse',  // Don't forget to change to https if needed
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
   }
